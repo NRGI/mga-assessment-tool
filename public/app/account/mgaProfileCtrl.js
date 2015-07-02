eb3b9461ac0e47e337452794e8c08f2336615ace
Chris Perry <byndcivilization@gmail.com>
2015-07-02 15:26:47 -0400

    SAVEPOINT

--- public/app/account/mgaProfileCtrl.js
+++ public/app/account/mgaProfileCtrl.js
@@ -1,11 +1,11 @@
 'use strict';
 //var angular;
 
-angular.module('app').controller('mgaProfileCtrl', function ($scope, mgaIdentitySrvc, mgaUserMethodSrvc, mgaNotifier) {
+angular.module('app').controller('mgaProfileCtrl', function ($scope, $route, mgaIdentitySrvc, mgaUserMethodSrvc, mgaNotifier) {
     // set page resources to be those of the current identity
     $scope.fullName = mgaIdentitySrvc.currentUser.firstName + " " + mgaIdentitySrvc.currentUser.lastName;
-    $scope.fname = mgaIdentitySrvc.currentUser.firstName;
-    $scope.lname = mgaIdentitySrvc.currentUser.lastName;
+    $scope.first_name = mgaIdentitySrvc.currentUser.firstName;
+    $scope.last_name = mgaIdentitySrvc.currentUser.lastName;
     $scope.email = mgaIdentitySrvc.currentUser.email;
     $scope.username = mgaIdentitySrvc.currentUser.username;
     $scope.role = mgaIdentitySrvc.currentUser.role;
@@ -14,31 +14,35 @@ angular.module('app').controller('mgaProfileCtrl', function ($scope, mgaIdentity
     $scope.update = function () {
         // pass in update data
         var new_user_data = $scope.user;
+        if (!$scope.first_name || !$scope.last_name || !$scope.email) {
+            mgaNotifier.error('You must include a name and email!')
+        } else {
+            new_user_data.firstName = $scope.first_name;
+            new_user_data.lastName = $scope.last_name;
+            new_user_data.email = $scope.email;
 
-        new_user_data.firstName = $scope.fname;
-        new_user_data.lastName = $scope.lname;
-        new_user_data.email = $scope.email;
-        if ($scope.password) {
-            if ($scope.password_rep) {
-                if ($scope.password === $scope.password_rep) {
+            if ($scope.password) {
+                if (!$scope.password_rep) {
+                    mgaNotifier.error('You must confirm your password!');
+                } else if ($scope.password !== $scope.password_rep) {
+                    mgaNotifier.error('Passwords must match!');
+                } else {
                     new_user_data.password = $scope.password;
                     mgaUserMethodSrvc.updateUser(new_user_data).then(function () {
                         mgaNotifier.notify('Your user account has been updated');
+                        $route.reload();
                     }, function (reason) {
                         mgaNotifier.error(reason);
                     });
-                } else {
-                    mgaNotifier.error('Passwords must match!');
                 }
             } else {
-                mgaNotifier.error('You must confirm your password!');
+                mgaUserMethodSrvc.updateUser(new_user_data).then(function () {
+                    mgaNotifier.notify('Your user account has been updated');
+                    $route.reload();
+                }, function (reason) {
+                    mgaNotifier.error(reason);
+                });
             }
-        } else {
-            mgaUserMethodSrvc.updateUser(new_user_data).then(function () {
-                mgaNotifier.notify('Your user account has been updated');
-            }, function (reason) {
-                mgaNotifier.error(reason);
-            });
         }
     };
 });
--- test/tests/controllers/account/mgaProfileCtrl.js
+++ test/tests/controllers/account/mgaProfileCtrl.js
@@ -15,128 +15,122 @@ describe('mgaProfileCtrl', function () {
         username: 'USERNAME',
         role: 'USER'
     };
-    //
-    //beforeEach(inject(
-    //    function ($rootScope, $controller, _rgiIdentitySrvc_, _rgiUserMethodSrvc_, _rgiNotifier_) {
-    //        $scope = $rootScope.$new();
-    //        rgiIdentitySrvc = _rgiIdentitySrvc_;
-    //        rgiUserMethodSrvc = _rgiUserMethodSrvc_;
-    //        rgiNotifier = _rgiNotifier_;
-    //
-    //        currentUserBackUp = rgiIdentitySrvc.currentUser;
-    //        rgiIdentitySrvc.currentUser = dummyCurrentUser;
-    //
-    //        $controller('rgiProfileCtrl', {$scope: $scope});
-    //    }
-    //));
-    //
-    //it('initialize the fields by values got from identity service', function () {
-    //    $scope.fullName.should.be.equal(dummyCurrentUser.firstName + ' ' + dummyCurrentUser.lastName);
-    //    $scope.first_name.should.be.equal(dummyCurrentUser.firstName);
-    //    $scope.last_name.should.be.equal(dummyCurrentUser.lastName);
-    //    $scope.email.should.be.equal(dummyCurrentUser.email);
-    //    $scope.role.should.be.equal(dummyCurrentUser.role);
-    //    $scope.username.should.be.equal(dummyCurrentUser.username);
-    //    $scope.address.should.be.equal(dummyCurrentUser.address);
-    //});
-    //
-    //describe('#update', function() {
-    //    var userMethodUpdateCurrentUserStub, userMethodUpdateCurrentUserSpy, notifierMock;
-    //
-    //    beforeEach(function () {
-    //        $scope.first_name = 'UPDATED FIRST NAME';
-    //        $scope.last_name = 'UPDATED LAST NAME';
-    //        $scope.email = 'UPDATED EMAIL';
-    //        $scope.address = 'UPDATED ADDRESS';
-    //        $scope.password = null;
-    //        notifierMock = sinon.mock(rgiNotifier);
-    //    });
-    //
-    //    describe('POSITIVE CASE', function() {
-    //        beforeEach(function () {
-    //            userMethodUpdateCurrentUserSpy = sinon.spy(function() {
-    //                return {
-    //                    then: function(callback) {
-    //                        callback();
-    //                    }
-    //                };
-    //            });
-    //            userMethodUpdateCurrentUserStub = sinon.stub(rgiUserMethodSrvc, 'updateUser', userMethodUpdateCurrentUserSpy);
-    //            notifierMock.expects('notify').withArgs('Your user account has been updated');
-    //        });
-    //
-    //        it('submits updated user data & shows success notification', function() {
-    //            $scope.update();
-    //            userMethodUpdateCurrentUserSpy.withArgs({
-    //                firstName: $scope.first_name,
-    //                lastName: $scope.last_name,
-    //                email: $scope.email,
-    //                address: $scope.address
-    //            }).called.should.be.equal(true);
-    //        });
-    //
-    //        it('submits updated user data (including password, if it is not empty) & shows success notification', function() {
-    //            $scope.password = 'PASSWORD';
-    //            $scope.password_rep = 'PASSWORD';
-    //            $scope.update();
-    //            userMethodUpdateCurrentUserSpy.withArgs({
-    //                firstName: $scope.first_name,
-    //                lastName: $scope.last_name,
-    //                email: $scope.email,
-    //                address: $scope.address,
-    //                password: $scope.password
-    //            }).called.should.be.equal(true);
-    //        });
-    //    });
-    //
-    //    //TODO add test for mismatched passwords
-    //    describe('NEGATIVE CASE', function() {
-    //        var FAILURE_REASON = 'FAILURE REASON';
-    //
-    //        beforeEach(function () {
-    //            userMethodUpdateCurrentUserSpy = sinon.spy(function() {
-    //                return {
-    //                    then: function(callbackPositive, callbackNegative) {
-    //                        callbackNegative(FAILURE_REASON);
-    //                    }
-    //                };
-    //            });
-    //            userMethodUpdateCurrentUserStub = sinon.stub(rgiUserMethodSrvc, 'updateUser', userMethodUpdateCurrentUserSpy);
-    //            notifierMock.expects('notify').withArgs(FAILURE_REASON);
-    //        });
-    //
-    //        it('submits updated user data & shows failure notification', function() {
-    //            $scope.update();
-    //            userMethodUpdateCurrentUserSpy.withArgs({
-    //                firstName: $scope.first_name,
-    //                lastName: $scope.last_name,
-    //                email: $scope.email,
-    //                address: $scope.address
-    //            }).called.should.be.equal(true);
-    //        });
-    //
-    //        it('submits updated user data (including password, if it is not empty) & shows failure notification', function() {
-    //            $scope.password = 'PASSWORD';
-    //            $scope.password_rep = 'PASSWORD';
-    //            $scope.update();
-    //            userMethodUpdateCurrentUserSpy.withArgs({
-    //                firstName: $scope.first_name,
-    //                lastName: $scope.last_name,
-    //                email: $scope.email,
-    //                address: $scope.address,
-    //                password: $scope.password
-    //            }).called.should.be.equal(true);
-    //        });
-    //    });
-    //
-    //    afterEach(function () {
-    //        notifierMock.verify();
-    //        notifierMock.restore();
-    //        userMethodUpdateCurrentUserStub.restore();
-    //    });
-    //});
-    //
-    //afterEach(function () {
-    //    rgiIdentitySrvc.currentUser = currentUserBackUp;
-    //});
+
+    beforeEach(inject(
+        function ($rootScope, $controller, _mgaIdentitySrvc_, _mgaUserMethodSrvc_, _mgaNotifier_) {
+            $scope = $rootScope.$new();
+            mgaIdentitySrvc = _mgaIdentitySrvc_;
+            mgaUserMethodSrvc = _mgaUserMethodSrvc_;
+            mgaNotifier = _mgaNotifier_;
+
+            currentUserBackUp = mgaIdentitySrvc.currentUser;
+            mgaIdentitySrvc.currentUser = dummyCurrentUser;
+
+            $controller('mgaProfileCtrl', {$scope: $scope});
+        }
+    ));
+
+    it('initialize the fields by values got from identity service', function () {
+        $scope.fullName.should.be.equal(dummyCurrentUser.firstName + ' ' + dummyCurrentUser.lastName);
+        $scope.first_name.should.be.equal(dummyCurrentUser.firstName);
+        $scope.last_name.should.be.equal(dummyCurrentUser.lastName);
+        $scope.email.should.be.equal(dummyCurrentUser.email);
+        $scope.role.should.be.equal(dummyCurrentUser.role);
+        $scope.username.should.be.equal(dummyCurrentUser.username);
+    });
+
+    describe('#update', function () {
+        var userMethodUpdateCurrentUserStub, userMethodUpdateCurrentUserSpy, notifierMock;
+
+        beforeEach(function () {
+            $scope.first_name = 'UPDATED FIRST NAME';
+            $scope.last_name = 'UPDATED LAST NAME';
+            $scope.email = 'UPDATED EMAIL';
+            $scope.password = null;
+            notifierMock = sinon.mock(mgaNotifier);
+        });
+
+        describe('POSITIVE CASE', function () {
+            beforeEach(function () {
+                userMethodUpdateCurrentUserSpy = sinon.spy(function () {
+                    return {
+                        then: function (callback) {
+                            callback();
+                        }
+                    };
+                });
+                userMethodUpdateCurrentUserStub = sinon.stub(mgaUserMethodSrvc, 'updateUser', userMethodUpdateCurrentUserSpy);
+                notifierMock.expects('notify').withArgs('Your user account has been updated');
+            });
+            //TODO fixt this test
+            //it('submits updated user data & shows success notification', function() {
+            //    $scope.update();
+            //    userMethodUpdateCurrentUserSpy.withArgs({
+            //        firstName: $scope.first_name,
+            //        lastName: $scope.last_name,
+            //        email: $scope.email
+            //    }).called.should.be.equal(true);
+            //});
+
+            //it('submits updated user data (including password, if it is not empty) & shows success notification', function() {
+            //    $scope.password = 'PASSWORD';
+            //    $scope.password_rep = 'PASSWORD';
+            //    $scope.update();
+            //    userMethodUpdateCurrentUserSpy.withArgs({
+            //        firstName: $scope.first_name,
+            //        lastName: $scope.last_name,
+            //        email: $scope.email,
+            //        password: $scope.password
+            //    }).called.should.be.equal(true);
+            //});
+        });
+
+        //TODO add test for mismatched passwords
+        describe('NEGATIVE CASE', function () {
+            var FAILURE_REASON = 'FAILURE REASON';
+
+            beforeEach(function () {
+                userMethodUpdateCurrentUserSpy = sinon.spy(function () {
+                    return {
+                        then: function (callbackPositive, callbackNegative) {
+                            callbackNegative(FAILURE_REASON);
+                        }
+                    };
+                });
+                userMethodUpdateCurrentUserStub = sinon.stub(mgaUserMethodSrvc, 'updateUser', userMethodUpdateCurrentUserSpy);
+                notifierMock.expects('notify').withArgs(FAILURE_REASON);
+            });
+            //
+            //it('submits updated user data & shows failure notification', function() {
+            //    $scope.update();
+            //    userMethodUpdateCurrentUserSpy.withArgs({
+            //        firstName: $scope.first_name,
+            //        lastName: $scope.last_name,
+            //        email: $scope.email
+            //    }).called.should.be.equal(true);
+            //});
+
+            //it('submits updated user data (including password, if it is not empty) & shows failure notification', function() {
+            //    $scope.password = 'PASSWORD';
+            //    $scope.password_rep = 'PASSWORD';
+            //    $scope.update();
+            //    userMethodUpdateCurrentUserSpy.withArgs({
+            //        firstName: $scope.first_name,
+            //        lastName: $scope.last_name,
+            //        email: $scope.email,
+            //        password: $scope.password
+            //    }).called.should.be.equal(true);
+            //});
+        });
+
+        afterEach(function () {
+            notifierMock.verify();
+            notifierMock.restore();
+            userMethodUpdateCurrentUserStub.restore();
+        });
+    });
+
+    afterEach(function () {
+        mgaIdentitySrvc.currentUser = currentUserBackUp;
+    });
 });
