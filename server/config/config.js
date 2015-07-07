d39cc7fdd367c50a19622d6806b72cdeaaeee73f
Chris Perry <byndcivilization@gmail.com>
2015-07-07 14:51:17 -0400

    SAVEPOINT

--- server/config/config.js
+++ server/config/config.js
@@ -9,12 +9,13 @@ module.exports = {
         port: process.env.PORT || 3030
     },
     development: {
-        db: '@candidate.32.mongolayer.com:10726/mga_dev',
+        // db: '@candidate.32.mongolayer.com:10726/mga_dev',
+        db: 'c726.candidate.19.mongolayer.com:10726,candidate.32.mongolayer.com:10582/mga_dev?replicaSet=set-54c2868c4ae1de388800b2a3',
         rootPath: rootPath,
         port: process.env.PORT || 80
     },
     production: {
-        db: '@candidate.32.mongolayer.com:10582/mga_production',
+        db: '@candidate.32.mongolayer.com:10726/mga_production',
         rootPath: rootPath,
         port: process.env.PORT || 80
     }
