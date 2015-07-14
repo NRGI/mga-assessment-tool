6b5a9d6c170e44871c95c51c903eb5b9d279cefa
Chris Perry <byndcivilization@gmail.com>
2015-07-14 15:12:57 -0400

    handeled duplicate assessment check

--- public/app/dialogs/mgaNewAssessmentDialogCtrl.js
+++ public/app/dialogs/mgaNewAssessmentDialogCtrl.js
@@ -12,6 +12,13 @@ angular.module('app').controller('mgaNewAssessmentDialogCtrl', function ($scope,
         return number + ""; // always return a string
     }
 
+    $scope.assessments = [];
+    $scope.$parent.assessments.forEach(function (el) {
+        $scope.assessments.push(el.assessment_ID);
+
+    });
+
+
     $scope.new_assessment = {
         year: "",
         assessment_country: {}
@@ -32,81 +39,101 @@ angular.module('app').controller('mgaNewAssessmentDialogCtrl', function ($scope,
         ngDialog.close();
     };
     $scope.assessmentDeploy = function () {
-        var new_assessment_data = [],
-            new_answer_data = [],
-            timestamp = new Date().toISOString(),
-            question_set_length = 0;
+        if ($scope.assessments.indexOf($scope.new_assessment.assessment_country.iso2 + '-' + String($scope.new_assessment.year)) > -1) {
+            mgaNotifier.error('Assessment already exists!');
+        } else {
+            var new_assessment_data = [],
+                new_answer_data = [],
+                timestamp = new Date().toISOString(),
+                question_set_length = 0;
 
-        mgaQuestionSrvc.query({assessment_ID: 'base'}, function (data) {
+            mgaQuestionSrvc.query({assessment_ID: 'base'}, function (data) {
 
-            data.forEach(function (el) {
-                new_answer_data.push({
-                    root_question_ID: el._id,
-                    answer_ID: $scope.new_assessment.assessment_country.iso2 + '-' + String($scope.new_assessment.year) + '-' + String(zeroFill(el.question_flow_order, 3)),
-                    year: String($scope.new_assessment.year),
+                data.forEach(function (el) {
+                    new_answer_data.push({
+                        root_question_ID: el._id,
+                        answer_ID: $scope.new_assessment.assessment_country.iso2 + '-' + String($scope.new_assessment.year) + '-' + String(zeroFill(el.question_flow_order, 3)),
+                        year: String($scope.new_assessment.year),
+                        assessment_ID: $scope.new_assessment.assessment_country.iso2 + '-' + String($scope.new_assessment.year),
+                        country: $scope.new_assessment.assessment_country.country,
+                        status: 'created',
+                        question_text: el.question_text,
+                        question_mode: el.question_mode,
+                        question_data_type: el.question_data_type,
+                        question_order: el.question_order,
+                        question_flow_order: el.question_flow_order,
+                        question_indicator: el.question_indicator,
+                        question_indicator_ID: el.question_indicator_ID,
+                        question_theme_ID: el.question_theme_ID,
+                        question_value_chain_ID: el.question_value_chain_ID,
+                        answer_text: "",
+                        answer_options: [],
+                        modified: [{
+                            modifiedBy: $scope.$parent.identity.currentUser._id,
+                            modifiedDate: timestamp
+                        }]
+                    });
+
+                    if (el.question_mode === 'desk_research') {
+                        question_set_length += 1;
+                    }
+                    if (el.question_data_type === "score") {
+                        new_answer_data[new_answer_data.length - 1]['answer_options'].push({
+                            option_order: 1,
+                            option_text: 'Yes',
+                            value: 4
+                        });
+                        new_answer_data[new_answer_data.length - 1]['answer_options'].push({
+                            option_order: 2,
+                            option_text: 'Partial',
+                            value: 2.5
+                        });
+                        new_answer_data[new_answer_data.length - 1]['answer_options'].push({
+                            option_order: 3,
+                            option_text: 'No',
+                            value: 1
+                        });
+                        new_answer_data[new_answer_data.length - 1]['answer_options'].push({
+                            option_order: 4,
+                            option_text: 'N/A',
+                            value: 0
+                        });
+                    }
+                });
+                new_assessment_data.push({
                     assessment_ID: $scope.new_assessment.assessment_country.iso2 + '-' + String($scope.new_assessment.year),
+                    ISO3: $scope.new_assessment.assessment_country.country_ID,
                     country: $scope.new_assessment.assessment_country.country,
+                    year: $scope.new_assessment.year,
                     status: 'created',
-                    question_text: el.question_text,
-                    question_mode: el.question_mode,
-                    question_data_type: el.question_data_type,
-                    question_order: el.question_order,
-                    question_flow_order: el.question_flow_order,
-                    question_indicator: el.question_indicator,
-                    question_indicator_ID: el.question_indicator_ID,
-                    question_theme_ID: el.question_theme_ID,
-                    question_value_chain_ID: el.question_value_chain_ID,
-                    answer_text: "",
-                    answer_options: [],
+                    questions_flagged: 0,
+                    questions_unfinalized: data.length,
+                    question_length: data.length,
+                    question_set_length: question_set_length,
                     modified: [{
-                        modifiedBy: $scope.$parent.identity.currentUser._id,
-                        modifiedDate: timestamp
-                    }]
+                        modified_by: $scope.$parent.identity.currentUser._id,
+                        modified_date: timestamp
+                    }],
+                    create_date: {
+                        created_by: $scope.$parent.identity.currentUser._id,
+                        date: timestamp
+                    }
                 });
+                console.log(question_set_length);
+                //send to mongo
+                mgaAssessmentMethodSrvc.createAssessment(new_assessment_data)
+                    .then(mgaAnswerMethodSrvc.insertAnswerSet(new_answer_data))
+                    .then(function () {
+                        mgaNotifier.notify('Assessment deployed!');
+                        new_assessment_data = undefined;
+                        new_answer_data = undefined;
+                        $scope.closeThisDialog();
+                        $route.reload();
+                    }, function (reason) {
+                        mgaNotifier.error(reason);
+                    });
 
-                if (el.question_mode === 'desk_research') {
-                    question_set_length += 1;
-                }
-                if (el.question_data_type === "score") {
-                    new_answer_data[new_answer_data.length - 1]['answer_options'].push({option_order: 1, option_text: 'Yes', value: 4});
-                    new_answer_data[new_answer_data.length - 1]['answer_options'].push({option_order: 2, option_text: 'Partial', value: 2.5});
-                    new_answer_data[new_answer_data.length - 1]['answer_options'].push({option_order: 3, option_text: 'No', value: 1});
-                    new_answer_data[new_answer_data.length - 1]['answer_options'].push({option_order: 4, option_text: 'N/A', value: 0});
-                }
             });
-            new_assessment_data.push({
-                assessment_ID: $scope.new_assessment.assessment_country.iso2 + '-' + String($scope.new_assessment.year),
-                ISO3: $scope.new_assessment.assessment_country.country_ID,
-                country: $scope.new_assessment.assessment_country.country,
-                year: $scope.new_assessment.year,
-                status: 'created',
-                questions_flagged: 0,
-                questions_unfinalized: data.length,
-                question_length: data.length,
-                question_set_length: question_set_length,
-                modified: [{
-                    modified_by: $scope.$parent.identity.currentUser._id,
-                    modified_date: timestamp
-                }],
-                create_date: {
-                    created_by: $scope.$parent.identity.currentUser._id,
-                    date: timestamp
-                }
-            });
-            console.log(question_set_length);
-            //send to mongo
-            mgaAssessmentMethodSrvc.createAssessment(new_assessment_data)
-                .then(mgaAnswerMethodSrvc.insertAnswerSet(new_answer_data))
-                .then(function () {
-                    mgaNotifier.notify('Assessment deployed!');
-                    new_assessment_data = undefined;
-                    new_answer_data = undefined;
-                    $scope.closeThisDialog();
-                    $route.reload();
-                }, function (reason) {
-                    mgaNotifier.error(reason);
-                });
-
-        });
+        }
     };
 });
